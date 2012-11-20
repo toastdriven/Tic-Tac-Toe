3b8551649f65b64b39c3159a0015d96cda3e3473
Daniel Lindsley <daniel@toastdriven.com>
2012-11-20 01:34:29 -0600

    Added a working & tested board implementation.

---
+++ tictactoe/board.py
@@ -0,0 +1,102 @@
+class BoardException(Exception):
+    pass
+
+
+class OutOfBounds(BoardException):
+    pass
+
+
+class InvalidMove(BoardException):
+    pass
+
+
+class Board(object):
+    default_size = 3
+
+    def __init__(self, size=None):
+        super(Board, self).__init__()
+
+        if not size or size < 1:
+            size = self.default_size
+
+        self.width = size
+        self.height = size
+
+        # Initialize the board.
+        # We start at top-left, indexing with x then y.
+        self.board = [[None for i in range(self.height)] for j in range(self.width)]
+
+    def within_bounds(self, x, y):
+        if x < 0:
+            raise OutOfBounds("{0} must be greater than or equal to 0.".format(x))
+
+        if y < 0:
+            raise OutOfBounds("{0} must be greater than or equal to 0.".format(y))
+
+        if x >= self.width:
+            raise OutOfBounds("{0} must be less than {1}.".format(x, self.width))
+
+        if y >= self.height:
+            raise OutOfBounds("{0} must be less than {1}.".format(y, self.height))
+
+        return True
+
+    def play(self, x, y, marker='X'):
+        self.within_bounds(x, y)
+
+        if not marker in ('X', 'O'):
+            raise InvalidMove("Silly! Only X's & O's are allowed.")
+
+        if self.board[x][y] is not None:
+            raise InvalidMove("You can't play there. There's already a {0} there.".format(self.board[x][y]))
+
+        self.board[x][y] = marker
+        return True
+
+    def check_for_win(self):
+        # Check the horizontals.
+        for y in range(self.height):
+            results = [self.board[x][y] for x in range(self.width)]
+
+            if not None in results:
+                if len(set(results)) == 1:
+                    # They're all the same value. A win!
+                    return results[0]
+
+        # Check the verticals.
+        for x in range(self.width):
+            results = [self.board[x][y] for y in range(self.height)]
+
+            if not None in results:
+                if len(set(results)) == 1:
+                    # They're all the same value. A win!
+                    return results[0]
+
+        # Check the TL->BR diagonal.
+        results = [self.board[off][off] for off in range(self.width)]
+
+        if not None in results:
+            if len(set(results)) == 1:
+                # They're all the same value. A win!
+                return results[0]
+
+        # Check the BL->TR diagonal.
+        results = [self.board[self.width - off - 1][off] for off in range(self.width - 1, -1, -1)]
+
+        if not None in results:
+            if len(set(results)) == 1:
+                # They're all the same value. A win!
+                return results[0]
+
+        # No win found.
+        return False
+
+    def moves_left(self):
+        moves = self.width * self.height
+
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.board[x][y] is not None:
+                    moves -= 1
+
+        return moves
---
+++ tictactoe/tests/test_board.py
@@ -0,0 +1,150 @@
+import unittest2
+from ..board import Board, OutOfBounds, InvalidMove
+
+
+class BoardTestCase(unittest2.TestCase):
+    def setUp(self):
+        super(BoardTestCase, self).setUp()
+        self.board = Board()
+
+    def test_standard_init(self):
+        board = Board()
+        self.assertEqual(board.width, 3)
+        self.assertEqual(board.height, 3)
+        self.assertEqual(board.board, [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ])
+
+    def test_smaller_init(self):
+        board = Board(size=2)
+        self.assertEqual(board.width, 2)
+        self.assertEqual(board.height, 2)
+        self.assertEqual(board.board, [
+            [None, None],
+            [None, None],
+        ])
+
+    def test_negative_init(self):
+        board = Board(size=-2)
+        self.assertEqual(board.width, 3)
+        self.assertEqual(board.height, 3)
+        self.assertEqual(board.board, [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ])
+
+    def test_within_bounds(self):
+        self.assertTrue(self.board.within_bounds(0, 0))
+        self.assertTrue(self.board.within_bounds(1, 1))
+        # Test the extremes.
+        self.assertTrue(self.board.within_bounds(0, 2))
+        self.assertTrue(self.board.within_bounds(2, 0))
+        self.assertTrue(self.board.within_bounds(2, 2))
+        # Test ouside.
+        self.assertRaises(OutOfBounds, self.board.within_bounds, 0, -1)
+        self.assertRaises(OutOfBounds, self.board.within_bounds, -1, 0)
+        self.assertRaises(OutOfBounds, self.board.within_bounds, 0, 3)
+        self.assertRaises(OutOfBounds, self.board.within_bounds, 3, 0)
+
+    def test_play(self):
+        self.board.board = [
+            ['X', None, 'O'],
+            ['X', 'X', 'O'],
+            ['O', None, None],
+        ]
+
+        # There's already a marker there.
+        self.assertRaises(InvalidMove, self.board.play, 0, 0, 'O')
+        self.assertRaises(InvalidMove, self.board.play, 2, 0, 'O')
+        # Open spot, invalid marker.
+        self.assertRaises(InvalidMove, self.board.play, 2, 2, 'Z')
+
+        self.assertTrue(self.board.play(2, 2, 'X'))
+
+    def test_moves_left(self):
+        self.assertEqual(self.board.moves_left(), 9)
+
+        self.board.board = [
+            ['X', None, 'O'],
+            ['X', 'X', 'O'],
+            ['O', None, None],
+        ]
+        self.assertEqual(self.board.moves_left(), 3)
+
+        self.board.board = [
+            ['X', 'O', 'O'],
+            ['X', 'X', 'O'],
+            ['O', 'X', 'O'],
+        ]
+        self.assertEqual(self.board.moves_left(), 0)
+
+    def test_check_for_win(self):
+        # Empty board, no one has won.
+        self.assertFalse(self.board.check_for_win())
+
+        # No wins yet.
+        self.board.board = [
+            ['X', None, 'O'],
+            ['X', 'X', 'O'],
+            ['O', None, None],
+        ]
+        self.assertFalse(self.board.check_for_win())
+
+        # X wins horizontally.
+        self.board.board = [
+            ['X', 'X', 'X'],
+            ['X', 'O', 'O'],
+            ['O', 'O', None],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'X')
+        self.board.board = [
+            ['X', 'O', 'O'],
+            ['X', 'X', 'X'],
+            ['O', 'O', None],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'X')
+        self.board.board = [
+            ['X', 'O', 'O'],
+            ['O', 'O', None],
+            ['X', 'X', 'X'],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'X')
+
+        # O wins vertically.
+        self.board.board = [
+            ['O', 'X', 'X'],
+            ['O', 'X', 'O'],
+            ['O', 'O', None],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'O')
+        self.board.board = [
+            ['X', 'O', 'O'],
+            ['X', 'O', 'X'],
+            ['O', 'O', None],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'O')
+        self.board.board = [
+            ['X', 'O', 'O'],
+            ['O', None, 'O'],
+            ['X', 'X', 'O'],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'O')
+
+        # X wins diagonally.
+        self.board.board = [
+            ['X', None, 'O'],
+            ['X', 'X', 'O'],
+            ['O', None, 'X'],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'X')
+
+        # O wins diagonally.
+        self.board.board = [
+            ['X', None, 'O'],
+            ['X', 'O', 'O'],
+            ['O', None, 'X'],
+        ]
+        self.assertEqual(self.board.check_for_win(), 'O')
